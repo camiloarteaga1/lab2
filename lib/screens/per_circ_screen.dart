21258f00e6f69f069f698ac6b32b7a25be7863c4
EasierMonk95 <popesinho@gmail.com>
2024-09-23 17:16:46 -0500

    The perimeter calculator screen was created.

---
+++ lib/screens/per_circ_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PerCircScreen extends StatefulWidget {
+  const PerCircScreen({super.key});
+
+  @override
+  State<PerCircScreen> createState() => _PerCircScreenState();
+}
+
+class _PerCircScreenState extends State<PerCircScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
